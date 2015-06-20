d213a142ac181817418f6cf237d70b175245f118
clemons <mrelusive@gmail.com>
2015-06-20 09:59:49 -0500

    Chapter 1 Exercises

--- chapter1-introduction/exercises/TwoThreads.java
+++ chapter1-introduction/exercises/TwoThreads.java
@@ -1,10 +1,62 @@
 import java.util.concurrent.*;
 
 public class TwoThreads {
+	static class SumThread extends Thread {
+      private int a[];
+      private int sum;
+
+      public SumThread(int a[]) {
+         this.a = a;
+         this.sum = 0;
+      }
+
+      public void run() {
+         for (int i = 0; i < a.length; i++) {
+            this.sum += this.a[i];
+         } 
+      }
+
+      public int getResult() {
+         return this.sum;
+      }
+	}
+	static class MaxThread extends Thread {
+      private int a[];
+      private int max;
+
+      public MaxThread(int a[]) {
+         this.a = a;
+         this.max = -1;
+      }
+
+      public void run() {
+         for (int i = 0; i < a.length; i++) {
+            if (a[i] > max) {
+               max = a[i];
+            }
+         } 
+      }
+
+      public int getResult() {
+         return max;
+      }
+	}
+
 	public static void main(String[] args){
 		int a[] = {1,2,3,4,5,6,7,8,9,10};
-		// TODO: start two threads that compute the sum and the max of the array entries in parallel
-		// print them out
+      MaxThread maxThread = new TwoThreads.MaxThread(a);
+      SumThread sumThread = new TwoThreads.SumThread(a);
+      maxThread.start();
+      sumThread.start();
 
+      try {
+         maxThread.join();
+         sumThread.join();
+      } catch (InterruptedException e)
+      {
+      }
+
+      System.out.println("Max: " + maxThread.getResult());
+      System.out.println("Sum: " + sumThread.getResult());
 	}
 }
