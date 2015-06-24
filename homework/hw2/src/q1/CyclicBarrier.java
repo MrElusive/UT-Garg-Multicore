b3348674183e4dfbad9e9e8a40cca389d68c1937
clemons <mrelusive@gmail.com>
2015-06-24 02:33:23 -0500

    Removed the broken CyclicBarrier

--- homework/hw2/src/q1/CyclicBarrier.java
+++ homework/hw2/src/q1/CyclicBarrier.java
@@ -1,88 +1,79 @@
 import java.util.concurrent.Semaphore;
 
 public class CyclicBarrier {
-  // TODO: Declare variables and the constructor for CyclicBarrier
-  // Note that you can use only semaphores but not synchronized blocks and
-  // locks 
+   private static boolean DEBUG = false;
+
    int parties;
    volatile int numThreadsArrived;
-   Semaphore[] barrier;
+   Semaphore barrier;
    Semaphore mutex;
-   volatile int currentBarrier;
 
   public CyclicBarrier(int parties) {
-    // TODO: The constructor for this CyclicBarrier
     this.parties = parties;
     numThreadsArrived = 0;
-    barrier = new Semaphore[2];
-    barrier[0] = new Semaphore(0);
-    barrier[1] = new Semaphore(0);
+    final boolean isFair = true;
+    barrier = new Semaphore(0, isFair);
     mutex = new Semaphore(1);
-    currentBarrier = 0;
   }
-
+  
+  // Waits until all parties have invoked await on this barrier.
+  // If the current thread is not the last to arrive then it is
+  // disabled for thread scheduling purposes and lies dormant until
+  // the last thread arrives.
+  // Returns: the arrival index of the current thread, where index
+  // (parties - 1) indicates the first to arrive and zero indicates
+  // the last to arrive.
   public int await() throws InterruptedException {
-    // Waits until all parties have invoked await on this barrier.
-    // If the current thread is not the last to arrive then it is
-    // disabled for thread scheduling purposes and lies dormant until
-    // the last thread arrives.
-    // Returns: the arrival index of the current thread, where index
-    // (parties - 1) indicates the first to arrive and zero indicates
-    // the last to arrive.
-	  //barrier[1-currentBarrier].acquire();
 	  mutex.acquire();
+	  
 	  numThreadsArrived++;
 	  int arrivalIndex = parties-numThreadsArrived;
-	  int myBarrier = currentBarrier;
 	  
 	  if (arrivalIndex == 0) {
-		  System.out.println("Let's go, everybody!");
-		  barrier[currentBarrier].release(parties-1);
-		  currentBarrier = 1-currentBarrier;
+		  if (DEBUG) System.out.println("Let's go, everybody!");
+		  barrier.release(parties-1);
 		  numThreadsArrived = 0;
 	  } else {
-		  System.out.println("I was blocked at barrier: " + myBarrier);		  
+		  if (DEBUG) System.out.println("I was blocked!");		  
 	  }
+	  
 	  mutex.release();
 	  
 	  if (arrivalIndex != 0) {
-		  barrier[myBarrier].acquire();
+		  barrier.acquire();
 	  }
-	  
-	  //System.out.println("I'm leaving barrier: " + currentBarrier);
-	  
+	  	  
 	  return arrivalIndex;
   }
     
   public static void main(String[] args) {
+   if (args.length > 0) {
+      if (args[0].equals("DEBUG")) {
+         CyclicBarrier.DEBUG = true;
+      }
+   }
 	int parties = 10;
+	int numThreads = 100;
 	final CyclicBarrier cyclicBarrier = new CyclicBarrier(parties);
-	Thread[] threads = new Thread[parties];
+	Thread[] threads = new Thread[numThreads];
 	
 	Runnable doSomething = new Runnable() {
 		public void run() {
 			try {
 				while (true) {
-					int arrivalIndex = cyclicBarrier.await();
-//					System.out.println(
-//						String.format("Thread %d exited with arrivalIndex=%d", 
-//							Thread.currentThread().getId(), 
-//							arrivalIndex
-//						)
-//					);
+					cyclicBarrier.await();
 				}
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 	};
 	
-	for (int i = 0; i < parties; i++) {
+	for (int i = 0; i < numThreads; i++) {
 		threads[i] = new Thread(doSomething);
 	}
 	
-	for (int i = 0; i < parties; i++) {
+	for (int i = 0; i < numThreads; i++) {
 		threads[i].start();
 	}
   }
